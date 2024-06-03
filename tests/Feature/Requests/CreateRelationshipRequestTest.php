c64a246aa827611f702502d840b6e6719e3512ea
Niels <niels.vermeiren@esign.eu>
2024-06-03 11:19:49 +0200

    Create relationship request + test

---
+++ tests/Feature/Requests/CreateRelationshipRequestTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Esign\Plytix\Tests\Feature\Requests;
+
+use Esign\Plytix\Plytix;
+use Esign\Plytix\Requests\CreateProductCategoryRequest;
+use Esign\Plytix\Requests\CreateRelationshipRequest;
+use Esign\Plytix\Tests\Support\MockResponseFixture;
+use Esign\Plytix\Tests\TestCase;
+use Saloon\Http\Faking\MockClient;
+
+class CreateRelationshipRequestTest extends TestCase
+{
+    /** @test */
+    public function it_can_send_a_create_relationship_request()
+    {
+        $plytix = new Plytix();
+        $mockClient = MockClient::global([
+            MockResponseFixture::make(fixtureName: 'token.json', status: 200),
+            MockResponseFixture::make(fixtureName: 'create-relationship.json', status: 201),
+        ]);
+
+        $response = $plytix->send(new CreateRelationshipRequest([
+            'name' => '12345'
+        ]));
+
+        $relationship = $response->dto()[0];
+        
+        $mockClient->assertSent(CreateRelationshipRequest::class);
+        $this->assertEquals('12345', $relationship->name);
+        $this->assertEquals('12345', $relationship->label);
+    }
+}
