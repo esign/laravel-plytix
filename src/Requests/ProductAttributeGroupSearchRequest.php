105ef4053001fee16faaf58dea9b143bd1690c43
Jordy Van der Haegen <jordy@esign.eu>
2024-07-12 09:05:47 +0200

    Add: product attribute group search reqeust

---
+++ src/Requests/ProductAttributeGroupSearchRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Esign\Plytix\Requests;
+
+use Esign\Plytix\DataTransferObjects\ProductAttributeGroup;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\PaginationPlugin\Contracts\Paginatable;
+use Saloon\Traits\Body\HasJsonBody;
+
+class ProductAttributeGroupSearchRequest extends Request implements HasBody, Paginatable
+{
+    use HasJsonBody;
+
+    protected Method $method = Method::POST;
+
+    public function resolveEndpoint(): string
+    {
+        return '/api/v1/attribute-groups/product/search';
+    }
+
+    public function createDtoFromResponse(Response $response): mixed
+    {
+        return array_map(function (array $productAttributeGroup) {
+            return ProductAttributeGroup::from($productAttributeGroup);
+        }, $response->json('data'));
+    }
+}
