116fd8fb44882bab45daedc002277b33cd6650cd
jordyvanderhaegen <jordy@esign.eu>
2025-01-27 20:44:49 +0100

    chore: include /api/v2/products/<product-id>/assets

---
+++ src/Requests/V2/LinkAssetToProductRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Esign\Plytix\Requests\V2;
+
+use Esign\Plytix\DataTransferObjects\V2\Asset;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\Traits\Body\HasJsonBody;
+
+class LinkAssetToProductRequest extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    protected Method $method = Method::POST;
+
+    public function __construct(
+        protected string $productId,
+        protected array $payload
+    ) {
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return '/api/v2/products/' . $this->productId . "/assets";
+    }
+
+    public function defaultBody(): array
+    {
+        return $this->payload;
+    }
+
+    public function createDtoFromResponse(Response $response): mixed
+    {
+        return array_map(function (array $asset) {
+            return Asset::from($asset);
+        }, $response->json('data'));
+    }
+}
