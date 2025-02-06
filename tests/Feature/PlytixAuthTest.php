ce13a5657d4aee08b956a397f4b1e9b6fcff82bf
jordyvanderhaegen <jordy@esign.eu>
2025-02-06 12:53:06 +0100

    fix: plytix uses separate rate limiting for api and auth

--- tests/Feature/PlytixAuthTest.php
+++ tests/Feature/PlytixAuthTest.php
@@ -2,30 +2,22 @@
 
 namespace Esign\Plytix\Tests\Feature;
 
-use Esign\Plytix\Enums\RateLimitingPlan;
 use Esign\Plytix\PlytixAuth;
+use Esign\Plytix\Tests\Support\AssertsRateLimits;
 use Esign\Plytix\Tests\TestCase;
-use Illuminate\Support\Facades\Config;
 use Saloon\RateLimitPlugin\Limit;
 
 class PlytixAuthTest extends TestCase
 {
+    use AssertsRateLimits;
+
     /** @test */
-    public function it_can_use_the_rate_limiting_plan_defined_in_the_config(): void
+    public function it_can_respect_rate_limiting(): void
     {
-        Config::set('plytix.rate_limiting.plan', RateLimitingPlan::PAID);
         $connector = new PlytixAuth();
 
         $limits = $connector->getLimits();
 
-        $this->assertLimitsContain(limits: $limits, allow: 20, releaseInSeconds: 10);
-        $this->assertLimitsContain(limits: $limits, allow: 5000, releaseInSeconds: 3600);
-    }
-
-    protected function assertLimitsContain(array $limits, int $allow, int $releaseInSeconds): void
-    {
-        $this->assertTrue(collect($limits)->contains(function (Limit $limit) use ($allow, $releaseInSeconds) {
-            return $limit->getAllow() === $allow && $limit->getReleaseInSeconds() === $releaseInSeconds;
-        }));
+        $this->assertLimitsContain(limits: $limits, allow: 8, releaseInSeconds: 1);
     }
 }
